ee2cbb860da91ed775c9ce023cbda37971d5d174
lies_amouldering <3goliad@gmail.com>
2019-01-22 17:33:54 -0500

    log received state

--- main.rb
+++ main.rb
@@ -7,6 +7,7 @@ require_relative "./syncer"
 
 post "/sync" do
   observed = JSON.parse(request.body.read)
+  logger.info "recieved state: #{JSON.generate(observed)}"
   desired = Syncer.new(observed).desired
   logger.info "desired state: #{JSON.generate(desired)}"
   json desired
