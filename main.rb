becadf980449490269d16c734b79792eb01d6fa6
lies_amouldering <3goliad@gmail.com>
2019-01-23 11:54:20 -0500

    avoid logging sensitive information

--- main.rb
+++ main.rb
@@ -7,8 +7,6 @@ require_relative "./syncer"
 
 post "/sync" do
   observed = JSON.parse(request.body.read)
-  logger.info "recieved state: #{JSON.generate(observed)}"
   desired = Syncer.new(observed).desired
-  logger.info "desired state: #{JSON.generate(desired)}"
   json desired
 end
